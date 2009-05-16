addc67bb30bbedb66d194037fb695591128ac1ac
Greg Hewgill <greg@hewgill.com>
2009-05-17 00:01:12 +1200

    license

--- mkinst.py
+++ mkinst.py
@@ -1,3 +1,21 @@
+# Script to make the instruction jump table for emulino
+# Copyright 2009 Greg Hewgill
+#
+# This file is part of Emulino.
+#
+# Emulino is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Emulino is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Emulino.  If not, see <http://www.gnu.org/licenses/>.
+
 def bitmask(a):
     return sum(2**x for x in range(len(a)) if a[x])
 
